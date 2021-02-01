384519e77d5449f352dd89760d2e39a9e0de919c
MD SHAMIMUL AREFIN <shamimularefin100@gmail.com>
2021-02-01 22:14:42 +0600

    Add files via upload

---
+++ advanced-javascript/array-split.js
@@ -0,0 +1,10 @@
+const nums = [1,2,3,4,5,6,7,8];
+const part = nums.slice(2, 5);
+const removed = nums.splice(2,4, 77, 88, 99);
+
+console.log(part);
+console.log(nums);
+console.log(removed);
+
+const together = nums.join("");
+console.log(together);
---
+++ advanced-javascript/null-vs-undefined.js
@@ -0,0 +1,22 @@
+// let pakhi;
+
+
+// function add(num1, num2){
+//     ///console.log(num1 + num2);
+//     /// return num1 + num2;
+// }
+// const result = add(12, 13);
+
+
+// function add(num1, num2){
+//     console.log(num1, num2);
+// }
+// const result = add(13);
+
+
+// let fun = undefined;
+// console.log(fun);
+
+//----------------------------------------------------------------
+
+null
